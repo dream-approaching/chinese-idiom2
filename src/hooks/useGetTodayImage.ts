34d2c4be5f13028e19fe25e1f886ce7bdf686d47
zhengjinshou <zhengjinshou@qtrade.com.cn>
2022-12-19 17:48:20 +0800

    添加换图按钮

--- src/hooks/useGetTodayImage.ts
+++ src/hooks/useGetTodayImage.ts
@@ -6,24 +6,37 @@ import defaultTopImage from '@/assets/images/defaultTop.jpg';
 let todayImage = '';
 const useGetTodayImage = () => {
   const [image, setImage] = useState<string | null>(todayImage);
+  const [imgLoading, setImgLoading] = useState(false);
 
   useEffect(() => {
     if (image) return;
 
+    getImageAction({});
+  }, []);
+
+  const refreshImage = () => {
+    getImageAction({ withTimeStamp: true });
+  };
+
+  const getImageAction = (data) => {
+    setImgLoading(true);
     HttpRequest({
       url: IdiomApi.getTodayImage,
+      data,
     })
       .then((res) => {
         todayImage = res as string;
         setImage(res as string);
+        setImgLoading(false);
       })
       .catch((err) => {
         setImage(defaultTopImage);
+        setImgLoading(false);
         console.log(err);
       });
-  }, []);
+  };
 
-  return image;
+  return { image, refreshImage, imgLoading };
 };
 
 export default useGetTodayImage;
--- src/pages/solitaire/components/header/index.tsx
+++ src/pages/solitaire/components/header/index.tsx
@@ -1,14 +1,16 @@
 import { View, Text } from '@tarojs/components';
 import dayjs from 'dayjs';
+import { AtActivityIndicator } from 'taro-ui';
 import { useGetTodayImage } from '@/hooks/index';
 import { IdiomBelong, Max_Idiom_Time } from '@/config/constants';
 import styles from './index.module.less';
 
 const SolitaireHeader = ({ currentSolitaireList, showRight, onRestart }) => {
-  const todayImage = useGetTodayImage();
+  const { image, refreshImage, imgLoading } = useGetTodayImage();
 
   return (
-    <View className={styles.headerCardCon} style={{ backgroundImage: `url(${todayImage})` }}>
+    <View className={styles.headerCardCon} style={{ backgroundImage: `url(${image})` }}>
+      <AtActivityIndicator isOpened={imgLoading} mode="center"></AtActivityIndicator>
       <View className={styles.leftPart}>
         <View className={styles.titleCon}>
           <Text className={styles.title}>成语接龙</Text>
@@ -16,7 +18,12 @@ const SolitaireHeader = ({ currentSolitaireList, showRight, onRestart }) => {
             游戏规则: 成语的最后一个字和下一个成语的第一个字必须相同，且成语长度必须大于等于4个字。最长等待时间{Max_Idiom_Time}秒
           </Text>
         </View>
-        <Text className={styles.footer}>{dayjs().format('MMMM D, YYYY')}</Text>
+        <View className={styles.footer}>
+          <Text className={styles.time}>{dayjs().format('MMMM D, YYYY')}</Text>
+          <Text className={styles.toggleBtn} onClick={refreshImage}>
+            换背景图
+          </Text>
+        </View>
       </View>
       {showRight && (
         <View className={styles.rightPart}>
