4b907711b93ab921f96f15bd31fc4638d3e86895
zhengjinshou <zhengjinshou@qtrade.com.cn>
2022-12-20 16:21:18 +0800

    feat: 所有列表

--- src/components/solitaire-item/index.tsx
+++ src/components/solitaire-item/index.tsx
@@ -1,20 +1,25 @@
 import { View, Text } from '@tarojs/components';
-import { generalColorByStr } from '@/utils/index';
 import type { TypeSolitaireItem } from '@/types/http-types/common';
 import { IdiomBelong, ColorTheme } from '@/config/constants';
 import { AtActivityIndicator } from 'taro-ui';
 import styles from './index.module.less';
 
-export default function SolitaireItem({ item }: { item?: TypeSolitaireItem }) {
+export default function SolitaireItem({ item, size }: { item?: TypeSolitaireItem; size: string }) {
+  const avatarBg = item?.effect ? (item?.belong === IdiomBelong.robot ? ColorTheme.robot : ColorTheme.user) : 'transparent';
   return (
-    <View className={styles.itemCon} key={item?.idiom.word}>
+    <View className={`${styles.itemCon} ${styles[size]}`} key={item?.idiom.word}>
       <View className={styles.pinyinAvatar}>
-        <View className={styles.avatar} style={{ backgroundColor: generalColorByStr(item?.idiom.pinyin.split(' ')[0] || '') }}>
+        <View className={styles.avatar} style={{ backgroundColor: avatarBg }}>
           {item?.idiom.pinyin.split(' ')[0]}
         </View>
       </View>
       <View className={styles.idiomContent}>
-        {(item?.idiom.word && <Text className={styles.idiomText}>{item.idiom.word}</Text>) || <Text className={styles.idiomTextWaiting}>小灵思考中。。。</Text>}
+        {(item?.idiom.word && (
+          <Text className={`${styles.idiomText} ${item.effect ? '' : styles.unEffect}`}>
+            {item.idiom.word}
+            {!item.effect && '(无效)'}
+          </Text>
+        )) || <Text className={styles.idiomTextWaiting}>小灵思考中。。。</Text>}
         <Text className={styles.idiomPinyin}>{item?.idiom.pinyin}</Text>
       </View>
       <View className={styles.rightContent}>
--- src/pages/solitaire/components/header/index.tsx
+++ src/pages/solitaire/components/header/index.tsx
@@ -1,49 +1,74 @@
 import { View, Text } from '@tarojs/components';
 import dayjs from 'dayjs';
-import { AtActivityIndicator } from 'taro-ui';
+import { useState } from 'react';
+import { AtActivityIndicator, AtDrawer } from 'taro-ui';
 import { useGetTodayImage } from '@/hooks/index';
 import { IdiomBelong, Max_Idiom_Time } from '@/config/constants';
+import { SolitaireItem } from '@/components';
+import type { TypeSolitaireItem } from '@/types/http-types/common';
 import styles from './index.module.less';
 
 const SolitaireHeader = ({ currentSolitaireList, showRight, onRestart }) => {
   const { image, refreshImage, imgLoading } = useGetTodayImage();
 
+  const [showDraw, setShowDraw] = useState(false);
+  const [showList, setShowList] = useState<TypeSolitaireItem[]>([]);
+  const handleShowDrawer = (type: string) => {
+    setShowDraw(true);
+    if (type === 'all') {
+      setShowList(currentSolitaireList);
+    }
+    if (type === 'mine') {
+      setShowList(currentSolitaireList.filter((item) => item.belong === IdiomBelong.user));
+    }
+  };
+  const handleHideDrawer = () => {
+    setShowDraw(false);
+  };
+
   return (
-    <View className={styles.headerCardCon} style={{ backgroundImage: `url(${image})` }}>
-      <AtActivityIndicator isOpened={imgLoading} mode="center"></AtActivityIndicator>
-      <View className={styles.leftPart}>
-        <View className={styles.titleCon}>
-          <Text className={styles.title}>成语接龙</Text>
-          <Text className={styles.rule}>
-            游戏规则: 成语的最后一个字和下一个成语的第一个字必须相同，且成语长度必须大于等于4个字。最长等待时间{Max_Idiom_Time}秒
-          </Text>
-        </View>
-        <View className={styles.footer}>
-          <Text className={styles.time}>{dayjs().format('MMMM D, YYYY')}</Text>
-          <Text className={styles.toggleBtn} onClick={refreshImage}>
-            换背景图
-          </Text>
-        </View>
-      </View>
-      {showRight && (
-        <View className={styles.rightPart}>
-          <View className={styles.rightTop}>
-            <View className={styles.lenCon}>
-              <Text className={styles.length}>{currentSolitaireList.length}</Text>
-              <Text className={styles.lengthTip}>本局</Text>
-            </View>
-            <View className={styles.lenCon}>
-              <Text className={styles.length}>{currentSolitaireList.filter((item) => item.belong === IdiomBelong.user && item.effect).length}</Text>
-              <Text className={styles.lengthTip}>我的</Text>
-            </View>
+    <View className={styles.headContainer}>
+      <View className={styles.headerCardCon} style={{ backgroundImage: `url(${image})` }}>
+        <AtActivityIndicator isOpened={imgLoading} mode="center"></AtActivityIndicator>
+        <View className={styles.leftPart}>
+          <View className={styles.titleCon}>
+            <Text className={styles.title}>成语接龙</Text>
+            <Text className={styles.rule}>
+              游戏规则: 成语的最后一个字和下一个成语的第一个字必须相同，且成语长度必须大于等于4个字。最长等待时间{Max_Idiom_Time}秒
+            </Text>
           </View>
-          <View className={styles.rightBottom}>
-            <Text className={styles.restartText} onClick={onRestart}>
-              重新开始
+          <View className={styles.footer}>
+            <Text className={styles.time}>{dayjs().format('MMMM D, YYYY')}</Text>
+            <Text className={styles.toggleBtn} onClick={refreshImage}>
+              换背景图
             </Text>
           </View>
         </View>
-      )}
+        {showRight && (
+          <View className={styles.rightPart}>
+            <View className={styles.rightTop}>
+              <View className={styles.lenCon} onClick={() => handleShowDrawer('all')}>
+                <Text className={styles.length}>{currentSolitaireList.length}</Text>
+                <Text className={styles.lengthTip}>本局</Text>
+              </View>
+              <View className={styles.lenCon} onClick={() => handleShowDrawer('mine')}>
+                <Text className={styles.length}>{currentSolitaireList.filter((item) => item.belong === IdiomBelong.user && item.effect).length}</Text>
+                <Text className={styles.lengthTip}>我的</Text>
+              </View>
+            </View>
+            <View className={styles.rightBottom}>
+              <Text className={styles.restartText} onClick={onRestart}>
+                重新开始
+              </Text>
+            </View>
+          </View>
+        )}
+      </View>
+      <AtDrawer show={showDraw} mask onClose={handleHideDrawer} right width="66vw" className={styles.drawContent}>
+        {showList.map((item) => (
+          <SolitaireItem key={item.idiom.word} item={item} size="small" />
+        ))}
+      </AtDrawer>
     </View>
   );
 };
